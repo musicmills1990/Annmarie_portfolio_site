4d321f4b89532108092567909ba0dfe46c57500e
Marc Mills <mdmills1990@gmail.com>
2019-11-08 12:09:28 -0500

    adds project routes, project controller and file organization

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :home
   root "home#main"
   get '/about' => "home#about"
-  get '/portfolio' => "home#portfolio"
+  get '/portfolio' => "projects#index"
+  get '/engagement' => "projects#engagement"
   get '/contact' => "home#contact"
-
+  get '/bridal' => "projects#bridal"
+  get '/wedding' => "projects#wedding"
 end
